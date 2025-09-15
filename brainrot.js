3c4bf1fdafd4ba752c2f468a4c601e649e0da7ae
Very lixo <ryofreefire32@gmail.com>
2025-09-15 11:59:52 -0300

    Update brainrot.js

--- brainrot.js
+++ brainrot.js
@@ -1,30 +1,29 @@
-AddElement("Brainrot", {
-    color: "#28d232",
-    state: "liquid",
-    behavior: "liquid",
-    category: "Special",
-    colorBG: "#F5E100",
-    weight: 1
-})
-
-AddElement("IceMangos", {
+elements.brainrot = {
+  color: ["#28d232"],
+  behavior: behaviors.LIQUID,
+  category: "special",
+  state: "liquid",
+  density: 1,
+}
+elements.IceMangos = {
     color: "#9fc6f2",
     state: "solid",
-    behavior: "solid",
+    behavior: behaviors.SOLID,
     weight: 5
-})
+}
 
-AddElement("Mangos", {
-    color: "#eaa95f",
-    category: "Food",
+
+elements.mangos = {
+    color: ["#eaa95f"],
+    category: "food",
     state: "solid",
-    behavior: "solid",
+    behavior: behaviors.SOLID,
     tempHigh: 100,
     tempLow: -10,
     stateLow: "IceMangos",
     colorBG: "#F5E100",
     weight: 3,
     reactions: {
-        "Brainrot": { "elem2": "Mustard", "chance": 100 }
+    "Brainrot": {"elem2": "Mustard", "chance": 100}
     }
-})
+}
