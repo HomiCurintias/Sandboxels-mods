308bcae361845c43490a92ba20c3df2f5878bfe7
Very lixo <ryofreefire32@gmail.com>
2025-09-14 22:52:40 -0300

    Update brainrot.js

--- brainrot.js
+++ brainrot.js
@@ -1,18 +1,21 @@
 addElement("Brainrot", {
     color: "#28d232",
     state: "liquid",
+    behavior: "liquid",
     category: "Special"
 })
 
 addElement("IceMangos", {
     color: "#9fc6f2",
-    state: "solid"
+    state: "solid",
+    behavior: "solid"
 })
 
 addElement("Mangos", {
     color: "#eaa95f",
     category: "Food",
     state: "solid",
+    behavior: "solid",
     tempHigh: 100,
     tempLow: -10,
     stateLow: "IceMangos",
