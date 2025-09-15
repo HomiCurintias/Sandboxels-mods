8b40ed21f13d4f69264aa9fb7f8766552bf6b7e6
Very lixo <ryofreefire32@gmail.com>
2025-09-15 14:11:10 -0300

    Update brainrot.js

--- brainrot.js
+++ brainrot.js
@@ -1,4 +1,4 @@
-elements.brainrot = {
+elements.Brainrot = {
   color: ["#28d232"],
   behavior: behaviors.LIQUID,
   category: "special",
@@ -6,31 +6,31 @@ elements.brainrot = {
   density: 1,
   movable: true,
   temp: 0,
-},
+};
 
-elements.iceMangos = {
-    color: "#9fc6f2",
-    state: "solid",
-    behavior: behaviors.SOLID,
-    weight: 5,
-    movable: true,
-    temp: -70,
-    tempHigh: 0,
-    stateHigh: ["mangos"],
-},
+elements.IceMangos = {
+  color: ["#9fc6f2"],
+  state: "solid",
+  behavior: behaviors.SOLID,
+  weight: 5,
+  movable: true,
+  temp: -70,
+  tempHigh: 0,
+  stateHigh: "Mangos",
+};
 
-elements.mangos = {
-    color: ["#eaa95f"],
-    category: "food",
-    state: "solid",
-    behavior: behaviors.SOLID,
-    tempLow: -10,
-    stateLow: ["IceMangos"],
-    colorBG: ["#F5E100"],
-    temp: 20,
-    weight: 3,
-    reactions: {
-    "Brainrot": {"elem2": "Mustard", "chance": 100}
-    },
-    movable: true,
-}
+elements.Mangos = {
+  color: ["#eaa95f"],
+  category: "food",
+  state: "solid",
+  behavior: behaviors.SOLID,
+  tempLow: -10,
+  stateLow: "IceMangos",
+  colorBG: ["#F5E100"],
+  temp: 20,
+  weight: 3,
+  reactions: {
+    "Brainrot": { elem2: "Mustard", chance: 100 }
+  },
+  movable: true,
+};
